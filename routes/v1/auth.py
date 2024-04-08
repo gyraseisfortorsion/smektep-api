00e48c446d669cde3ce2743f55e02d37af54e758
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2024-04-08 19:52:23 +0500

    add user post request

--- routes/v1/auth.py
+++ routes/v1/auth.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends, HTTPException, status, Response
-from fastapi.security import HTTPBearer
-# from fastapi_jwt_auth import AuthJWT
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.orm import Session
 
 from core import get_db
-from schemas import LoginForm
-from services import auth_service
+from schemas import LoginForm, UserCreate
+from services import auth_service, user_service
 
 router = APIRouter(prefix="/auth", tags=["Authorization"])
 
@@ -13,6 +12,10 @@ router = APIRouter(prefix="/auth", tags=["Authorization"])
 @router.post("/login")
 def login(form: LoginForm, db: Session = Depends(get_db)):
     return auth_service.login_for_access_token(form, db)
+
+@router.post("/register")
+def register(body: UserCreate, db: Session = Depends(get_db)):
+    return user_service.create(db, body)
 
 @router.post("/refresh-token")
 def refresh_token(refresh_token: str, db: Session = Depends(get_db)):
