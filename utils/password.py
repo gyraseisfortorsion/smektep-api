b57b6831cc504c398328e0cfdcd2be4306f4c4f0
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2024-04-08 21:19:42 +0500

    s'

--- utils/password.py
+++ utils/password.py
@@ -8,7 +8,10 @@ def hash_password(password: str):
     return pwd_context.hash(password)
 
 def verify_password(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+    try:
+        return pwd_context.verify(password, hashed_password)
+    except:
+        return False
 
 # def get_access_token_by_user_id(Authorize: AuthJWT,
 #                                        db: Session,
