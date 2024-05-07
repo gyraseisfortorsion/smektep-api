123b24e7e832451980bda88f10698dc7618c62df
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2024-05-07 06:38:51 +0500

    small bug fixes

--- services/test_gemini.py
+++ services/test_gemini.py
@@ -1,12 +1,12 @@
-import PIL.Image
-import google.generativeai as genai
+# import PIL.Image
+# import google.generativeai as genai
 
 
-GOOGLE_API_KEY = ""
+# GOOGLE_API_KEY = ""
 
-genai.configure(api_key=GOOGLE_API_KEY)
-img = PIL.Image.open('image.jpg')
-model = genai.GenerativeModel('gemini-pro-vision')
-response = model.generate_content(["Write a short, engaging blog post based on this picture. It should include a description of the meal in the photo and talk about my journey meal prepping.", img], stream=True)
-response.resolve()
-print(response.text)
+# genai.configure(api_key=GOOGLE_API_KEY)
+# img = PIL.Image.open('image.jpg')
+# model = genai.GenerativeModel('gemini-pro-vision')
+# response = model.generate_content(["Write a short, engaging blog post based on this picture. It should include a description of the meal in the photo and talk about my journey meal prepping.", img], stream=True)
+# response.resolve()
+# print(response.text)
