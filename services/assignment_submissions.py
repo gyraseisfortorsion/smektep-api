447df26ade589e6522ef364bbec534ffbd251ee2
askarali <56202525+gyraseisfortorsion@users.noreply.github.com>
2024-05-15 15:04:02 +0500

    fix path

--- services/assignment_submissions.py
+++ services/assignment_submissions.py
@@ -277,25 +277,26 @@ class AssignmentSubmissionService(ServiceBase[AssignmentSubmission, AssignmentSu
         #     max_tokens=500
         # )
 
-        transcribed_submission = model.generate_content(
+        # transcribed_submission = model.generate_content(
             
-            glm.Content(
-                parts = [
-                    glm.Part(text="First count how many answers are there, based on this count list all of the answers of the student, just the answers, if student couldnt solve or omitted the problem indicate that. REPLY IN RUSSIAN."),
-                    glm.Part(
-                        inline_data=glm.Blob(
-                            mime_type='image/jpeg',
-                            data=pathlib.Path('vertical_submission.jpg').read_bytes()
-                        )
-                    ),
+        #     glm.Content(
+        #         parts = [
+        #             glm.Part(text="First count how many answers are there, based on this count list all of the answers of the student, just the answers, if student couldnt solve or omitted the problem indicate that. REPLY IN RUSSIAN."),
+        #             glm.Part(
+        #                 inline_data=glm.Blob(
+        #                     mime_type='image/jpeg',
+        #                     data=pathlib.Path('vertical_submission.jpg').read_bytes()
+        #                 )
+        #             ),
                     
-                ],
-            ),
-            # generation_config=genai.types.GenerationConfig(
-            # max_output_tokens=0),
-            stream=True)
-        transcribed_submission.resolve()
-        print(transcribed_submission.text)
+        #         ],
+        #     ),
+        #     # generation_config=genai.types.GenerationConfig(
+        #     # max_output_tokens=0),
+        #     stream=True)
+        # transcribed_submission.resolve()
+        # print(transcribed_submission.text)
+
         assignment_description = db.query(Assignment).filter(Assignment.id == submission.assignment_id).first().description
         client = OpenAI(api_key=settings.OPENAI_API_KEY)
         response = client.chat.completions.create(
@@ -308,10 +309,10 @@ class AssignmentSubmissionService(ServiceBase[AssignmentSubmission, AssignmentSu
                 "type": "text",
                 "text": "First of all be sure to pay attention on student's answers and the correct answer list. Last image is the image with assignement and correct answers. Check the submitted work based on the provided answers, and provide detailed feedback and final mark. DONT FORGET TO PROVIDE FINAL MARK, IF ANY OF THE PROBLEMS OR QUESTIONS LEFT UNANSWERED IT MEANS NO POINT FOR THAT OR JUST INCORRECT ANSWER. REPLY IN RUSSIAN.",
                 },
-                {
-                "type": "text",
-                "text": f"Here is the transcribed solution of the student: {transcribed_submission.text}",
-                },
+                # {
+                # "type": "text",
+                # "text": f"Here is the transcribed solution of the student: {transcribed_submission.text}",
+                # },
                 {
                 "type": "image_url",
                 "image_url": {
